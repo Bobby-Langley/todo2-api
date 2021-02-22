40d2fc11596bb94b510bf155be3f69f4b4fca2ad
Bobby Langley <bobbylangley227@gmail.com>
2021-02-21 19:11:11 -0500

    got delete function to work

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -3,50 +3,88 @@ var serviceAccount = require("../credentials.json");
 
 let db;
 
-function dbAuth(){
-    if(!admin.apps.length){
+function dbAuth() {
+  if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+      credential: admin.credential.cert(serviceAccount),
     });
-    db = admin.firestore
-    }
+    db = admin.firestore();
+  }
 }
-    
+//
 exports.getTasks = (req, res) => {
-    dbAuth()
-    db.collection('tasks').get()
-        .then(collection => {
-            const taskList = collection.docs.map(doc=> {
-                let task = doc.data()
-                task.id = doc.id
-                return task
-            })
-            res.status(200).send('getTasks')
-        })
-        .catch(err=> res.status(500).send(err))
-}
+  if(!req.params.userId)
+      res.status(400).send('Invalid Request')
+  
+  dbAuth();
+  db.collection("tasks").where('userId', '==', req.params.userId).get()
+    .then((collection) => {
+      const taskList = collection.docs.map((doc) => {
+        let task = doc.data();
+        task.id = doc.id;
+        return task;
+      });
+      res.status(200).send(taskList);
+    })
+    .catch((err) => res.status(500).send("GET TASKS FAILED: " + err));
+};
 exports.postTask = (req, res) => {
-    if(!req.body || !req.body.item || !req.body.userId){
-        res.status(400).send('Invalid request')
-    }
-    dbAuth()
-    const newTask = {
-        item: req.body.item,
-        done: false,
-        userId: req.body.userId
-    }
-    db.collection('tasks').add(newTask)
-        .then(() => {
-            this.getTasks(req, res)
-        })
-        .catch(err => res.status(500).send(err))
-    res.status(200).send('postTasks')
-}
+  if (!req.body || !req.body.item || !req.body.userId || !req.params.userId) {
+    res.status(400).send("Invalid request");
+  }
+  dbAuth();
+  const newTask = {
+    item: req.body.item,
+    done: false,
+    userId: req.body.userId,
+  };
+  db.collection("tasks")
+    .add(newTask)
+    .then(() => {
+      this.getTasks(req, res);
+    })
+    .catch((err) => res.status(500).send("POST FAILED" + err));
+  
+};
 exports.patchTask = (req, res) => {
-    dbAuth()
-    res.status(200).send('patchTasks')
-}
+  dbAuth();
+  db.collection("tasks").doc(req.params.taskId).update()
+        .then(() => {
+            res.status(200).json({
+            status: 'successfully successful success',
+            message: 'Event updated',
+            statusCode: '204'
+            })
+        })
+        .catch(err => {
+            res.status(500).send ({
+                status: 'errrrrr',
+                data: err,
+                message: 'failed to update',
+                statusCode: '500'
+            })
+        })
+};
 exports.deleteTask = (req, res) => {
-    dbAuth()
-    res.status(200).send('deleteTasks')
-}
+  if(!req.params.taskId) {
+    res.status(400).send("Invalid request")
+  }
+  dbAuth();
+  db.collection("tasks").doc(req.params.taskId).delete()
+  .then(() => {
+    res.status(200).json({
+      status: 'successfully successful success',
+      message: 'Task deleted',
+      statusCode: '204'
+    })
+  })
+  .catch(err => {
+    res.status(500).send ({
+                status: 'errrrrr',
+                data: err,
+                message: 'Task not deleted',
+                statusCode: '500'
+              })
+            })
+            
+};
